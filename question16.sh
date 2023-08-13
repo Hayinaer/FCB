c1373336f8aa52b12f335a871f09aef6da279205
Hayinaer <142090861+Hayinaer@users.noreply.github.com>
2023-08-13 11:34:24 +0930

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -2,5 +2,5 @@ git clone https://github.com/ian-knight-uofa/git-practice-02.git
 cd git-practice-02
 git fetch origin
 git checkout branch2
-git merge branch3
-git branch -d branch3
+git merge origin/branch3
+git branch -d origin/branch3
