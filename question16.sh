7c34fa43d656e2a2578b224733e7b9d8b61b9982
Hayinaer <142090861+Hayinaer@users.noreply.github.com>
2023-08-13 14:48:22 +0930

    Delete question16.sh

--- question16.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/ian-knight-uofa/git-practice-02.git
-cd git-practice-02
-git fetch origin
-git checkout branch2
-git merge origin/branch3
-git branch -d --remote origin/branch3
