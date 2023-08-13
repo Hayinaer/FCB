b153448457091ed6bd56d38aa9f6a6bd3c30e4b6
Hayinaer <142090861+Hayinaer@users.noreply.github.com>
2023-08-13 14:48:10 +0930

    Delete question15.sh

--- question15.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/ian-knight-uofa/git-practice-01.git
-cd git-practice-01
-git fetch origin
-git checkout main
-git merge origin/branch1
-git add dir1/file3
-git add file1
-git commit -m "resolve"
-git merge origin/branch1
