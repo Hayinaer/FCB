7c2f19cc6f929f889d1f42f0fd855a48d0b5df61
Hayinaer <142090861+Hayinaer@users.noreply.github.com>
2023-08-13 11:47:48 +0930

    Update question13.sh

--- question13.sh
+++ question13.sh
@@ -1,7 +1,6 @@
 git checkout -b submission_branch
 git rm --cached *.sh
-git commit -m "question13"
 touch file13.txt
 git add file13.txt
 git commit -m "question13 add file13"
-git push origin/submission_branch
+git push submission_branch
