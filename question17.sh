ef34d160f49b4263480d99562350f281e8407d5d
Hayinaer <Hayinaeraierzhati@gmail.com>
2023-08-13 14:27:27 +0930

    a

--- question17.sh
+++ question17.sh
@@ -1,17 +1,22 @@
-git clone https://github.com/ian-knight-uofa/git-practice-03.git
-cd git-practice-03
-git fetch origin
-git checkout -b branch1
-rm -r dir2
-touch dir1/foo
-rm dir3/bar_copy
+#! /bin/bash
+git branch branch1
+git branch branch2
+git checkout main
+cp dir3/bar dir3/bar_copy
+git add .
+git commit -m "commit message"
+git checkout branch1
+mv dir1/dir2/foo dir1/
+rm -r dir1/dir2
 touch newfile1
 git add .
-git commit -m "Update branch1"
-git checkout -b branch2
+git commit -m "commit message"
+git checkout branch2
 mv dir1/dir2/foo dir1/dir2/foo_modified
 rm -r dir3
 mkdir dir1/dir3
 touch dir1/dir3/newfile2
 git add .
-git commit -m "Update branch2"
+git commit -m "commit message"
+
+
